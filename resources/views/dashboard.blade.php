c7f31992f5d17f86a5d01ce1101157bd370d25c0
vegaangga <vegaangga27@gmail.com>
2021-04-07 21:53:12 +0700

    update

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('index')
+
+@section('content')
+<div id="page-wrapper" >
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-md-12">
+                <p>Ini adalah dashboard</p>
+            </div>
+        </div>
+         <hr />
+    </div>
+
+</div>
+@endsection
